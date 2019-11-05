71c2605501399196513346a61d615f8600667459
adithyamaheshb <adithyamaheshbariki@gmail.com>
2019-11-04 20:54:28 -0500

    Working on Instagram Data

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { getHTML, getTwitterFollowers } from './lib/scraper';
 
 async function go() {
-    getTwitterFollowers(await getHTML('https://twitter.com/adithyamaheshb'));
+    const html = await getHTML('https://twitter.com/adithyamaheshb');
+    const twitterFollowers = await getTwitterFollowers(html);
+    console.log(`You have ${twitterFollowers} followers`);
 }
 
 go();
