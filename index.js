13ed2d3b864e5ce28f82285ebba74e0396976b0f
adithyamaheshb <adithyamaheshbariki@gmail.com>
2019-12-21 20:42:44 -0800

    save data to low db

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-import express from 'express';
-import { getInstagramCount, getTwitterCount } from './lib/scraper';
-import db from './lib/db';
+import express from "express";
+import { getInstagramCount, getTwitterCount } from "./lib/scraper";
+import db from "./lib/db";
 
 const app = express();
 
-console.log(db());
+app.get("/scrape", async (req, res, next) => {
+  console.log("Scraping!");
+  const [iCount, tCount] = await Promise.all([
+    getInstagramCount(),
+    getTwitterCount()
+  ]).catch(err => console.log(err));
+  console.log(iCount, tCount);
+  // console.log(twitter, instagram);
+  db.get("twitter")
+    .push({
+      date: Date.now(),
+      count: tCount
+    })
+    .write();
+  db.get("instagram")
+    .push({
+      date: Date.now(),
+      count: iCount
+    })
+    .write();
 
-app.get('/scrape', async (req, res, next) => {
-    console.log('Scraping!');
-    const [iCount, tCount] = await Promise.all([ getInstagramCount(), getTwitterCount() ]);
-    console.log(iCount, tCount);
-    res.json({ iCount, tCount });
+  res.json({ iCount, tCount });
 });
 
 app.listen(9672, () => {
-    console.log(`Example App is running on port 9672`);
-})
+  console.log(`Example App is running on port 9672`);
+});
